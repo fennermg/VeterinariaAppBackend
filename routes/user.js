644ccfd3ef79db5a8b737ac3a4ec72c46193af18
Obed Herrera <herreraobed0272@gmail.com>
2023-03-28 00:42:51 -0600

    API Mejorada (No cambiar)

--- routes/user.js
+++ routes/user.js
@@ -1,17 +1,35 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const collection = require("../models/User");
 
 router.get("/", async(req, res) => {
     const u = await User.findOne({user:req.body.user,password:req.body.password})
+    console.log(req.body)
     if (u) {
         res.send('Encontrado')
     }else{
         res.send('Invalido')
     }
+    
 });
 
-router.post("/", async (req, res) => {
+router.post("/", async(req,res)=>{
+  const{user, password}=req.body
+  try{
+    const check = await User.findOne({user:req.body.user,password:req.body.password})
+    if(check){
+      res.json("Encontrado")
+    }else{
+      res.json("Error")
+    }
+  }
+  catch(e){
+    res.json("Error")
+  }
+})
+
+router.post("/signup", async (req, res) => {
   const { user, password, role } = req.body;
 
   console.log(req.body);
