6257f2b47e5a46afb73c50d59c5fb2a4b7c82872
fennermg <fmenagaray@gmail.com>
2023-03-30 19:28:08 -0600

    console log de el request

--- routes/user.js
+++ routes/user.js
@@ -3,31 +3,31 @@ const router = express.Router();
 const User = require("../models/User");
 const collection = require("../models/User");
 
-router.get("/", async(req, res) => {
-    const u = await User.findOne({user:req.body.user,password:req.body.password})
-    console.log(req.body)
-    if (u) {
-        res.send('Encontrado')
-    }else{
-        res.send('Invalido')
-    }
-    
+router.get("/", async (req, res) => {
+  const u = await User.findOne({
+    user: req.body.user,
+    password: req.body.password,
+  });
+  console.log(req.body);
+  if (u) {
+    res.send("Encontrado");
+  } else {
+    res.send("Invalido");
+  }
 });
 
-router.post("/", async(req,res)=>{
-  const{user, password}=req.body
-  try{
-    const check = await User.findOne({user:req.body.user,password:req.body.password})
-    if(check){
-      res.json("Encontrado")
-    }else{
-      res.json("Error")
-    }
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  const check = await User.findOne({
+    user: req.body.user,
+    password: req.body.password,
+  });
+  if (check) {
+    res.json("Encontrado");
+  } else {
+    res.json("Error");
   }
-  catch(e){
-    res.json("Error")
-  }
-})
+});
 
 router.post("/signup", async (req, res) => {
   const { user, password, role } = req.body;
