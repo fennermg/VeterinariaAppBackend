33352bd75dc29218d8f4b444f3b11e84dd885fcb
Obed Herrera <herreraobed0272@gmail.com>
2023-04-25 00:42:36 -0600

    Fix menor a la API

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const app = express();
 
 connectDB()
 
-app.use(cors())
+app.use(cors({
+  origin: ["http://localhost:3000"],
+  credentials: true,
+}))
 app.use(express.json({ extented: false }));
 app.use(express.urlencoded())
 
