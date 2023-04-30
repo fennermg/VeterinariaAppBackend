ad6a9fc41dffc52a372fba44f7215a005c4e6dd1
fennermg <fmenagaray@gmail.com>
2023-04-30 17:41:25 -0600

    crud de paciente y responsable completo

--- routes/paciente.js
+++ routes/paciente.js
@@ -22,25 +22,21 @@ router.post("/", async (req, res) => {
   if (paciente.responsable) {
     try {
       await paciente.save().then((saved) => {
-        Responsable.findById(saved.responsable._id).then(
-          (responsable) => {
-            responsable.pacientes.push(saved);
-            responsable.save();
-          }
-        );
+        Responsable.findById(saved.responsable._id).then((responsable) => {
+          responsable.pacientes.push(saved);
+          responsable.save();
+        });
         res.status(200).send(saved);
       });
-
     } catch (error) {
       console.error(error);
       res.status(500).send(error.message);
     }
   } else {
     try {
-      await paciente.save().then((saved)=>{
+      await paciente.save().then((saved) => {
         res.status(200).send(saved);
       });
-
     } catch (error) {
       console.error(error);
       res.status(500).send(error.message);
@@ -48,68 +44,124 @@ router.post("/", async (req, res) => {
   }
 });
 
-
-
-router.get('/:id', (req, res) => {
-  Paciente.findById(req.params.id)
-    .then(paciente => {
+router.get("/:id", (req, res) => {
+  Paciente.findById(req.params.id).populate("responsable")
+    .then((paciente) => {
       if (paciente) {
         res.json(paciente);
       } else {
-        res.status(404).json({ error: 'Paciente not found' });
+        res.status(404).json({ error: "Paciente not found" });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json({ error: err.message });
     });
 });
 
+/*
 
-router.patch('/:id', async (req, res) => {
+router.patch("/:id", async (req, res) => {
   let pacienteBeforeUpdate = await Paciente.findById(req.params.id);
 
   Paciente.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then(paciente => {
+    .then((paciente) => {
       if (paciente) {
-          if (!paciente.responsable) {
+        if (!paciente.responsable) {
+          Responsable.findOneAndUpdate(
+            pacienteBeforeUpdate.responsable,
+            { $pull: { pacientes: paciente._id } },
+            { new: true }
+          );
+          res.json(paciente);
+        } else if (
+          paciente.responsable != pacienteBeforeUpdate.responsable
+        ) {
+          console.log("paso")
+          Responsable.findOneAndUpdate(
+            pacienteBeforeUpdate.responsable,
+            { $pull: { pacientes: paciente._id } },
+            { new: true }
+          );
 
-
-            Responsable.updateOne(
-              { _id: pacienteBeforeUpdate.responsable }, 
-              { $pull: { pacientes: paciente._id } }
-            );
-
-            
-            /*Responsable.findById(pacienteBeforeUpdate.responsable).then((found)=>{
-              console.log(found)
-              
-            })*/
-          }
-        res.json(paciente);
+          Responsable.findByIdAndUpdate(paciente.responsable._id).then(
+            (responsable) => {
+              responsable.pacientes.push(paciente);
+              responsable.save();
+            }
+          );
+          res.json(paciente);
+        }
       } else {
-        res.status(404).json({ error: 'Paciente not found' });
+        res.status(404).json({ error: "Paciente not found" });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json({ error: err.message });
     });
 });
 
+*/
 
-router.delete('/:id', (req, res) => {
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const pacienteBeforeUpdate = await Paciente.findById(req.params.id);
+    const updatedPaciente = await Paciente.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (updatedPaciente) {
+      if (!updatedPaciente.responsable) {
+        await Responsable.findOneAndUpdate(
+          pacienteBeforeUpdate.responsable,
+          { $pull: { pacientes: updatedPaciente._id } },
+          { new: true }
+        );
+      } else if (updatedPaciente.responsable != pacienteBeforeUpdate.responsable) {
+        await Responsable.findOneAndUpdate(
+          pacienteBeforeUpdate.responsable,
+          { $pull: { pacientes: updatedPaciente._id } },
+          { new: true }
+        );
+
+        const responsable = await Responsable.findByIdAndUpdate(
+          updatedPaciente.responsable._id,
+          { $push: { pacientes: updatedPaciente } },
+          { new: true }
+        );
+      }
+      res.json(updatedPaciente);
+    } else {
+      res.status(404).json({ error: "Paciente not found" });
+    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+
+router.delete("/:id", async (req, res) => {
+  const pacienteBeforeDelete = await Paciente.findById(req.params.id);
+
   Paciente.findByIdAndDelete(req.params.id)
-    .then(paciente => {
+    .then(async (paciente) => {
       if (paciente) {
-        res.json({ message: 'Paciente deleted' });
+        const responsable = await Responsable.findOneAndUpdate(
+          pacienteBeforeDelete.responsable,
+          { $pull: { pacientes: req.params.id } },
+          { new: true }
+        );
+        res.json({ message: "Paciente deleted" });
       } else {
-        res.status(404).json({ error: 'Paciente not found' });
+        res.status(404).json({ error: "Paciente not found" });
       }
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(400).json({ error: err.message });
     });
 });
-
 
 
 module.exports = router;
--- routes/responsable.js
+++ routes/responsable.js
@@ -60,46 +60,8 @@ router.get("/:id", (req, res) => {
     });
 });
 
-//Este metodo elimina mascotas y agrega pero tiene un error cuando se elimina la misma cantidad de mascotas que se grega.
+
 /*
-router.patch("/:id", async (req, res) => {
-  let responsableBeforeUpdate = await Responsable.findById(req.params.id);
-
-  Responsable.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((responsable) => {
-      if (responsable) {
-        const diffArr = responsableBeforeUpdate.pacientes.filter((item) => !responsable.pacientes.includes(item)).concat(responsable.pacientes.filter((item) => !responsableBeforeUpdate.pacientes.includes(item)));
-        if (responsableBeforeUpdate.pacientes.length > responsable.pacientes.length) {
-          console.log("Eliminando")
-          diffArr.forEach(async (paciente) => {
-            let found = await Paciente.findById(paciente._id);
-            found.responsable = undefined;
-            found.save();
-          });
-  
-          res.json(responsable);
-        }else{
-          console.log("agregando")
-          diffArr.forEach(async (paciente) => {
-            let found = await Paciente.findById(paciente._id);
-            found.responsable = responsable;
-            found.save();
-          });
-          res.json(responsable);
-        }
-
-      } else {
-        res.status(404).json({ error: "Responsable not found" });
-      }
-    })
-    .catch((err) => {
-      res.status(400).json({ error: err.message });
-    });
-});
-
-*/
-
-
 router.patch("/:id", async (req, res) => {
   let responsableBeforeUpdate = await Responsable.findById(req.params.id);
 
@@ -126,7 +88,49 @@ router.patch("/:id", async (req, res) => {
       res.status(400).json({ error: err.message });
     });
 });
+*/
 
+
+router.patch("/:id", async (req, res) => {
+  try {
+    let responsableBeforeUpdate = await Responsable.findById(req.params.id);
+
+    for (let paciente of responsableBeforeUpdate.pacientes) {
+      let found = await Paciente.findById(paciente._id).exec();
+      if (found) {
+        found.responsable = undefined;
+        await found.save();
+      } else {
+        throw new Error(`Paciente with id ${paciente._id} not found`);
+      }
+    }
+
+    Responsable.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      .then(async (responsable) => {
+        if (responsable) {
+          for (let paciente of responsable.pacientes) {
+            let found = await Paciente.findById(paciente._id).exec();
+            if (found) {
+              found.responsable = responsable;
+              await found.save();
+            } else {
+              throw new Error(`Paciente with id ${paciente._id} not found`);
+            }
+          }
+          res.json(responsable);
+        } else {
+          res.status(404).json({ error: "Responsable not found" });
+        }
+      })
+      .catch((err) => {
+        res.status(400).json({ error: err.message });
+      });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+/*
 router.delete("/:id", async (req, res) => {
   let responsableBeforeUpdate = await Responsable.findById(req.params.id);
 
@@ -147,6 +151,28 @@ router.delete("/:id", async (req, res) => {
     .catch((err) => {
       res.status(400).json({ error: err.message });
     });
+});
+*/
+
+router.delete("/:id", async (req, res) => {
+  try {
+    let responsableBeforeUpdate = await Responsable.findById(req.params.id);
+
+    for (let paciente of responsableBeforeUpdate.pacientes) {
+      let found = await Paciente.findById(paciente._id);
+      found.responsable = undefined;
+      await found.save();
+    }
+
+    let deletedResponsable = await Responsable.findByIdAndDelete(req.params.id);
+    if (deletedResponsable) {
+      res.json({ message: "Responsable deleted" });
+    } else {
+      res.status(404).json({ error: "Responsable not found" });
+    }
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
 });
 
 module.exports = router;
